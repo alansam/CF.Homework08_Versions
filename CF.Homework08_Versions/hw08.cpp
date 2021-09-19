3b7821dad45e09d090cf78ea1cf1740947638d27
Alan Sampson <alansamps@gmail.com>
2021-09-18 19:24:59 -0700

    Code cleanup 001.

--- CF.Homework08_Versions/hw08.cpp
+++ CF.Homework08_Versions/hw08.cpp
@@ -27,61 +27,66 @@
  */
 
 #include <iostream>
-#define size 2
+#include <string>
+//#define SIZE 2
+size_t constexpr SIZE { 2 };
 
 using namespace std;
-void riempimento (string n[],string c[],string tc[],string ce[],string ic[],string ie[],int taglia);
-void visualizza (string n[],string c[],string tc[],string ce[],string ic[],string ie[],int taglia);
-void bsort (string n[],string c[],string tc[],string ce[],string ic[],string ie[],int taglia);
+void riempimento(string n[], string c[], string tc[], string ce[], string ic[], string ie[], int taglia);
+void visualizza(string n[], string c[], string tc[], string ce[], string ic[], string ie[], int taglia);
+void bsort(string n[], string c[], string tc[], string ce[], string ic[], string ie[], int taglia);
 
 int main() {
-  string nome[size],cognome[size],telefono_casa[size],cellulare[size],indirizzo_casa[size],indirizzo_mail[size];
+  string nome[SIZE], cognome[SIZE], telefono_casa[SIZE], cellulare[SIZE], indirizzo_casa[SIZE], indirizzo_mail[SIZE];
   
-  riempimento(nome,cognome,telefono_casa,cellulare,indirizzo_casa,indirizzo_mail,size);
-  cout<<"\n\n Hai inserito:\n";
-  visualizza(nome,cognome,telefono_casa,cellulare,indirizzo_casa,indirizzo_mail,size);
-  bsort(nome,cognome,telefono_casa,cellulare,indirizzo_casa,indirizzo_mail,size);
-  cout<<"\n\nTe li ho ordinati cosi':\n";
-    visualizza(nome,cognome,telefono_casa,cellulare,indirizzo_casa,indirizzo_mail,size);
+  riempimento(nome, cognome, telefono_casa, cellulare, indirizzo_casa, indirizzo_mail, SIZE);
+  cout << "\n\n Hai inserito:\n";
+  visualizza(nome, cognome, telefono_casa, cellulare, indirizzo_casa, indirizzo_mail, SIZE);
+  bsort(nome, cognome, telefono_casa, cellulare, indirizzo_casa, indirizzo_mail, SIZE);
+  cout << "\n\nTe li ho ordinati cosi':\n";
+  visualizza(nome, cognome, telefono_casa, cellulare, indirizzo_casa, indirizzo_mail, SIZE);
   return 0;
 }
+
   //g=tc m=c a=ic
-void riempimento (string n[],string c[],string tc[],string ce[],string ic[],string ie[],int taglia){
-  for (int i=0;i<taglia;i++){ //scorrimento per tutta la lunghezza del vettore
-    cout<<"Inserisci il "<<i+1<<"^ nome: ";
-    cin>>n[i];
-    cout<<"Inserisci il "<<i+1<<"^ cognome: ";
-    cin>>c[i];
-        cout<<"Inserisci il "<<i+1<<"^ telefono di casa: ";
-    cin>>tc[i];
-    cout<<"Inserisci il "<<i+1<<"^ numero di cellulare: ";
-    cin>>ce[i];
-    cout<<"Inserisci il "<<i+1<<"^ indirizzo di casa: ";
-        cin>>ic[i];
-        cout<<"Inserisci il "<<i+1<<"^ indirizzo mail: ";
-        cin>>ie[i];
+void riempimento(string n[], string c[], string tc[], string ce[], string ic[], string ie[], int taglia) {
+  for (int i = 0; i < taglia; i++){ //scorrimento per tutta la lunghezza del vettore
+    cout << "Inserisci il " << i + 1 << "^ nome: ";
+    cin >> n[i];
+    cout << "Inserisci il " << i + 1 << "^ cognome: ";
+    cin >> c[i];
+    cout << "Inserisci il " << i + 1 << "^ telefono di casa: ";
+    cin >> tc[i];
+    cout << "Inserisci il " << i + 1 << "^ numero di cellulare: ";
+    cin >> ce[i];
+    cout << "Inserisci il " << i + 1 << "^ indirizzo di casa: ";
+    cin >> ic[i];
+    cout << "Inserisci il " << i + 1 << "^ indirizzo mail: ";
+    cin >> ie[i];
   }
 }
-void visualizza (string n[],string c[],string tc[],string ce[],string ic[],string ie[],int taglia){
-  for (int i=0;i<taglia;i++){
-        cout<<"Inserisci il "<<i+1<<"^ cognome: "<<c[i]<<endl;
-        cout<<"Inserisci il "<<i+1<<"^ nome: "<<n[i]<<endl;
-        cout<<"Inserisci il "<<i+1<<"^ telefono di casa: "<< tc[i]<<endl;
-    cout<<"Inserisci il "<<i+1<<"^ numero di cellulare: "<< ce[i]<<endl;
-        cout<<"Inserisci il "<<i+1<<"^ indirizzo email: "<< ie[i]<<endl<< endl;
 
+void visualizza(string n[], string c[], string tc[], string ce[], string ic[], string ie[], int taglia) {
+  for (int i = 0; i < taglia; i++) {
+    cout << "Inserisci il " << i + 1 << "^ cognome: " << c[i] << endl;
+    cout << "Inserisci il " << i + 1 << "^ nome: " << n[i] << endl;
+    cout << "Inserisci il " << i + 1 << "^ telefono di casa: " << tc[i] << endl;
+    cout << "Inserisci il " << i + 1 << "^ numero di cellulare: " << ce[i] << endl;
+    cout << "Inserisci il " << i + 1 << "^ indirizzo email: " << ie[i] << endl << endl;
   }
-    }
+}
 
-void bsort (string n[],string c[],string tc[],string ce[],string ic[],string ie[],int taglia){
-  for(int i=0;i<taglia-1;i++) //ciclo esterno che serve a vedere i giorni,mese e anno sono stati ordinati fino a quel momento
-      for(int j=0;j<taglia-1-i;j++) //ciclo interno che serve a confrontare ogni numero con il successivo e fa andare avanti i più grandi verso la fine e i più piccoli all'inizio
-      if((ic[j]==ic[j+1]&&c[j]==ce[j+1]&& tc[j]<tc[j+1])||(ic[j]==ic[j+1]&& c[j]<c[j+1])||(ic[j]<ic[j+1])){ //confronto tra anni,mesi e giorni. Se l'anno e il mese sono gli stessi controlla il giorno,se l'anno è lo stesso, controlla il mese o l'anno
-          swap(c[j+1],c[j]); //scambio
-          swap(n[j+1],n[j]); //scambio
-          swap(tc[j+1],tc[j]); //scambio
-          swap(c[j+1],ce[j]); //scambio
-          swap(ic[j+1],ic[j]); //scambio
-          }
+void bsort(string n[], string c[], string tc[], string ce[], string ic[], string ie[], int taglia) {
+  for (int i = 0; i < taglia - 1; i++) { //ciclo esterno che serve a vedere i giorni,mese e anno sono stati ordinati fino a quel momento
+    for (int j = 0; j < taglia - 1 - i; j++) { //ciclo interno che serve a confrontare ogni numero con il successivo e fa andare avanti i più grandi verso la fine e i più piccoli all'inizio
+      if ((ic[j] == ic[j + 1] && c[j] == ce[j + 1] && tc[j] < tc[j + 1]) || (ic[j] == ic[j + 1] && c[j] < c[j + 1]) || (ic[j] < ic[j + 1])) { //confronto tra anni,mesi e giorni. Se l'anno e il mese sono gli stessi controlla il giorno,se l'anno è lo stesso, controlla il mese o l'anno
+        swap(c[j + 1], c[j]); //scambio
+        swap(n[j + 1], n[j]); //scambio
+        swap(tc[j + 1], tc[j]); //scambio
+        swap(c[j + 1], ce[j]); //scambio
+        swap(ic[j + 1], ic[j]); //scambio
+      }
+    }
+  }
 }
   //g=tc m=c a=ic
